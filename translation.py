22f019c62064bf6b4f21994af3d39f68ba1e7391
Anthony Costanzo <anthonycostanzo23@gmail.com>
2020-10-28 19:23:39 -0400

    translator

---
+++ translation.py
@@ -0,0 +1,15 @@
+from googletrans import Translator
+translator = Translator()
+
+def translate(filename):
+    try:
+        with open(filename,mode='r+') as file:
+            lines = file.readlines()
+            for line in lines:
+                translated = translator.translate(line,dest='ja')
+                file.write(f'{line} translated = {translated.text}\n')
+                print(translated)
+    except(FileNotFoundError,IOError) as error:
+            print(f'It appears there was an error : {error}')
+
+translate('../folder/test1.txt')
